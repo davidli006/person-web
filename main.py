c565b79c4d11ab367f688169641dfc0eede303f3
李栋 <lidong65550@offcn.com>
2022-08-23 14:59:07 +0800

    update

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+@DATE: 2022/8/22
+@Author  : ld
+"""
+from flask import Flask, render_template
+
+from conf import HOST, PORT, DEBUG
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT, debug=DEBUG)
